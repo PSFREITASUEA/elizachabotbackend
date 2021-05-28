37f262d1bbda53f89f5d11ae4bdb64aac5da5497
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-27 21:42:12 -0400

    #Refactor: Adjust procfile and add index function

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify,request
+import os
 import time
 
 
 app = Flask(__name__)
 @app.route("/bot",method=["POST"])
 
+@app.route('/')
+def index():
+    return "<h1> Deployed to Heroku</h1>"
 
 #response
 def response():
@@ -13,5 +17,10 @@ def response():
     return jsonify({"response" : result})
 
 
+@app.route("/")
+def index():
+    return "<h1>Hello World</hi>"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
