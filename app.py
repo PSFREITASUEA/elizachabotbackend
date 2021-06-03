11929226c1efc727fb37b10635824efedb5a85a2
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-06-03 10:00:34 -0400

    Add port

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from os import name
+import os
 from flask import Flask, json, jsonify,request
 import tera
-import time
 
 app = Flask(__name__)
 
@@ -20,4 +20,5 @@ def getAnotherAnwsers(sentence):
     return jsonify({'response':tera.getAnswers(sentence)}) 
 
 if __name__ == "__main__":
-    app.run(debug= False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
