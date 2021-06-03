87ad450fbc94341ae8fe50aa35cdce29d1a0b286
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-06-03 14:34:25 -0400

    Update dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,12 @@
 welcome_message = {
-    "olá": "Olá, eu sou o TeraPy.É um prazer estar conversando com você.Em que posso ajudar?",
-    "oi": "Oi, eu sou o TeraPy. É um prazer estar conversando com você. Em que posso ajudar?"
+    "olá": "Olá, eu sou o Tera.Py.É um prazer estar conversando com você.Em que posso ajudar?",
+    "oi": "Oi, eu sou o Tera.Py. É um prazer estar conversando com você. Em que posso ajudar?"
 }
 
 feelings_message = {
     "estou me sentindo triste": "Por que você está se sentindo triste?",
     "estou me sentindo feliz": "Por que você está se sentindo feliz?",
+    "estou triste" : "Por que você está triste?",
     "estou me sentindo ansioso": "Por que você está se sentindo ansioso?",
     "estou sentindo medo": "Por que você está com medo?",
     "estou entediado": "Por que você está entediado?"
