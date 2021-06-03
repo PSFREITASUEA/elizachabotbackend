51a12074cd13e0dd01a32e38b99cf233c6af97ce
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-06-03 09:48:29 -0400

    Read all and add another function

--- app.py
+++ app.py
@@ -12,4 +12,12 @@ def index():
 @app.route("/bot/<sentence>", methods=['GET', 'POST'])
 def getAnswers(sentence):
     query = dict(request.form)['query']
+    return jsonify({'response':tera.getAnswers(query)}) 
+
+
+@app.route("/bot2/<sentence>")
+def getAnotherAnwsers(sentence):
     return jsonify({'response':tera.getAnswers(sentence)}) 
+
+if __name__ == "__main__":
+    app.run(debug= False)
